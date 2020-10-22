dda908c7817cbb3c32eb6873886410aad3479dcf
davhoon <294288551@qq.com>
2020-10-22 21:31:28 +0800

    flx 酒吧首页布局

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,7 +1,35 @@
 <template>
 	<view class="content" :style="{background:`url(${BGUrl})`}">
-		<view class="text-area">
-			<text class="title fontSize">{{title}}</text>
+		<view class="header">
+			<view class="logo"></view>
+			<view class="nameWapper">
+				<view class="title">上海双人舞酒吧</view>
+				<view class="number">当晚人数：769</view>
+			</view>
+			<view class="avatar">
+				<view class="item" style="background-color: #aa5500;"></view>
+				<view class="item" style="background-color: #00aa7f;"></view>
+				<view class="item" style="background-color: #aa55ff;"></view>
+			</view>
+		</view>
+		<view class="textWapper">
+			<view class="icon">Live</view>
+			<view class="text">Current Songs</view>
+		</view>
+		<view class="bottomWapper">
+			<view class="goodsWapper" >
+				<view class="goodsItem" @click="handleGoodsItem(item)" v-for="item in bottomList" :key="item.id">
+					<view class="icon" :class="[item.id == goodsItemActive ? 'iconHover' : '']">{{item.icon}}</view>
+					<view class="name">{{item.name}}</view>
+					<view class="price">{{item.price}}</view>
+				</view>
+			</view>
+			<view class="payment">
+				<view class="recharge">
+					{{recharge}} 充值>
+				</view>
+				<view class="submit">送给他</view>
+			</view>
 		</view>
 	</view>
 </template>
@@ -10,33 +38,66 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello',
-				BGUrl: '/static/images/bg.jpg'
+				goodsItemActive:null,
+				recharge:0,
+				BGUrl: '/static/images/bg.jpg',
+				bottomList:[
+					{
+						id:1,
+						icon: '111',
+						name:'四叶草',
+						price: 60
+					},{
+						id:2,
+						icon: '111',
+						name:'四叶草',
+						price: 123
+					},{
+						id:3,
+						icon: '111',
+						name:'四叶草',
+						price: 76
+					},{
+						id:6,
+						icon: '111',
+						name:'四叶草',
+						price: 79
+					},{
+						id:4,
+						icon: '111',
+						name:'四叶草',
+						price: 80
+					},{
+						id:5,
+						icon: '111',
+						name:'四叶草',
+						price: 123
+					},{
+						id:7,
+						icon: '111',
+						name:'四叶草',
+						price: 45
+					},{
+						id:8,
+						icon: '111',
+						name:'四叶草',
+						price: 79
+					}
+				]
 			}
 		},
 		onLoad() {},
-		methods: {}
+		methods: {
+			// 点击商品
+			handleGoodsItem(data){
+				this.goodsItemActive = data.id
+				this.recharge = data.price
+			}
+		}
 	}
 </script>
 
 <style>
-	/* @import url("../../static/style.css"); */
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		height: 100%;
-	}
-	
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		/* color: #8f8f94; */
-	}
+	/*每个页面公共css */
+	@import url("./style.css");
 </style>
